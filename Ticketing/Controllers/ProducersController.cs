34628a51c232ff96cea6dfc35331604a1176057e
MaragathaAtchaya <maragathaatchaya.m@gmail.com>
2023-08-08 20:28:52 +0800

    Basic controllers created

---
+++ Ticketing/Controllers/ProducersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ticketing.Data;
+
+namespace Ticketing.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }       
+
+        public async Task<IActionResult> Index()
+        {
+            var allproducers = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
